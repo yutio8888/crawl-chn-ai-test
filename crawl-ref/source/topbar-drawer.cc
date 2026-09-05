c0c60bdba3650f2490d19c90d3c11f8e7cc4a611
yutio8888 <sd168432@gmail.com>
2026-09-05 01:46:18 +0000

    fix(android): retain localized status descriptions without raw short text

--- crawl-ref/source/topbar-drawer.cc
+++ crawl-ref/source/topbar-drawer.cc
@@ -139,13 +139,7 @@ static formatted_string _build_status_text(int selected_status)
 
         text.textcolour(info.light_colour ? info.light_colour : LIGHTGREY);
         text += title;
-        if (!info.short_text.empty() && info.short_text != title)
-        {
-            text += "\n";
-            text += formatted_string::parse_string(info.short_text, LIGHTGREY);
-        }
-        if (!description.empty() && description != title
-            && description != info.short_text)
+        if (!description.empty() && description != title)
         {
             text += "\n";
             text += formatted_string::parse_string(description, LIGHTGREY);
